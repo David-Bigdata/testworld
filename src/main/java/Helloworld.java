52551046ed7ca1739147420660cfc7a986c50298
xiaoxin <david_bigdata@163.com>
2020-09-07 16:34:26 +0800

    你好

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -5,5 +5,7 @@
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("helloworld ");
+        System.out.println("你好");
+        System.out.println("nihao");
     }
 }
