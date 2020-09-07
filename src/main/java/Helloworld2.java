be5d6c5e30e38e24ed40c625e012bc0c86914447
xiaoxin <David_Bigdata@163.com>
2020-09-07 18:17:35 +0800

    新增部分

---
+++ src/main/java/Helloworld2.java
@@ -0,0 +1,12 @@
+
+/**
+ * @author xiaoxin
+ */
+public class Helloworld2 {
+    public static void main(String[] args) {
+        System.out.println("helloworld ");
+        System.out.println("你好");
+        System.out.println("nihao");
+        //新增1.2项目
+    }
+}
